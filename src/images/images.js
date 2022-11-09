c33307c5435a094dfd47e0481fac86cf8a0f9575
cat-miller <git@carolinmoeller.com>
2022-11-09 14:50:16 +0100

    initial

---
+++ src/images/images.js
@@ -0,0 +1,15 @@
+import dirtImg from './dirt.jpg';
+import glassImg from './glass.png';
+import grassImg from './grass.jpg';
+import logImg from './log.jpg';
+import woodImg from './wood.png';
+
+const images = {
+  dirtImg,
+  glassImg,
+  grassImg,
+  woodImg,
+  logImg,
+}
+
+export default images;
